4bc43b5359e96d29382db1e268782d985d27e8b9
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-23 17:40:39 -1000

    Setting up more dialogic elements

--- NPC/blacksmith.gd
+++ NPC/blacksmith.gd
@@ -1,13 +1,21 @@
 extends StaticBody2D
 
 var playerInArea = false
+var conversationInProgress = false
 
 func _ready():
-	pass
-
+	Dialogic.signal_event.connect(DialogicSignal)
+	
 func _process(delta):
-	if Input.is_action_just_pressed("talk") and playerInArea:
-		Dialogic.start("Blacksmith Smithing")
+	if Input.is_action_just_pressed("talk") and playerInArea and not conversationInProgress:
+		
+		#TODO check time and run a different dialogue
+		
+		run_dialogue("Blacksmith Smithing")
+	
+func run_dialogue(dialogue_string):
+	Dialogic.start(dialogue_string)
+	conversationInProgress = true
 
 func _on_player_detection_body_entered(body):
 	if body.has_method("player"):
@@ -16,7 +24,9 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.has_method("player"):
 		playerInArea = false
-
-func _on_dialogue_finished(dialogue_name):
-	if dialogue_name == "Blacksmith Smithing":
-		Dialogic.start("Evil")
+		conversationInProgress = false
+		
+func DialogicSignal(arg: String):
+	if arg == ("openShop"):
+		print("open shop function")
+		
