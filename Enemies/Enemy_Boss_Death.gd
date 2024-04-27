b555b73d247c61753a9ecd9cae51c6846cdd3620
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-26 15:36:44 -1000

    bug fixes

--- Enemies/Enemy_Boss_Death.gd
+++ Enemies/Enemy_Boss_Death.gd
@@ -3,7 +3,7 @@ extends Enemy_Death
 func Enter():
 	var enemyDeathEffect = EnemyDeathEffect.instantiate()
 	get_parent().add_child(enemyDeathEffect)
-	enemyDeathEffect.global_position = enemy.global_position
+	enemyDeathEffect.global_position = global_position
 	animation_player.play("death")
 	await animation_player.animation_finished
 	animation_player.play("boss_slain")
