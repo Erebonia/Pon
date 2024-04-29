9b4ea12229a8d1d0e64708af6c5cc7f695908a16
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-29 00:19:18 -1000

    Inventory continued..

--- Player/Inventory/inventory_gui.gd
+++ Player/Inventory/inventory_gui.gd
@@ -50,12 +50,21 @@ func close():
 	closed.emit()
 
 func onSlotClicked(slot):
-	if slot.isEmpty() and itemInHand:
+	if slot.isEmpty():
+		if !itemInHand: return
+		
 		insertItemInSlot(slot)
 		return
 		
 	if !itemInHand:
 		takeItemFromSlot(slot)
+		return
+		
+	if slot.itemStackGui.inventorySlot.item.name == itemInHand.inventorySlot.item.name:
+		stackItems(slot)
+		return
+		
+	swapItems(slot)
 	
 func takeItemFromSlot(slot):
 	itemInHand = slot.takeItem()
@@ -67,7 +76,36 @@ func insertItemInSlot(slot):
 	remove_child(itemInHand)
 	itemInHand = null
 	slot.insert(item)
+	
+func swapItems(slot):
+	var tempItem = slot.takeItem()
+	
+	insertItemInSlot(slot)
+	
+	itemInHand = tempItem
+	add_child(itemInHand)
+	updateItemInHand()
 
+func stackItems(slot):
+	var slotItem: ItemStackGui = slot.itemStackGui
+	var maxAmount = slotItem.inventorySlot.item.maxAmountPrStack
+	var totalAmount = slotItem.inventorySlot.amount + itemInHand.inventorySlot.amount
+	
+	if slotItem.inventorySlot.amount == maxAmount:
+		swapItems(slot)
+		return
+		
+	if totalAmount <= maxAmount:
+		slotItem.inventorySlot.amount = totalAmount
+		remove_child(itemInHand)
+		itemInHand = null
+	else:
+		slotItem.inventorySlot.amount = maxAmount
+		itemInHand.inventorySlot.amount = totalAmount - maxAmount
+		
+	slotItem.update()
+	if itemInHand: itemInHand.update()
+	
 func updateItemInHand():
 	if !itemInHand: return
 	itemInHand.global_position = get_global_mouse_position() - itemInHand.size / 2
