e78931ce743371b34dabacf71a0749d69a2051e5
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-01 21:15:03 -1000

    Setting up equipment inventory

--- Player/Inventory/Scripts/InventoryItem.gd
+++ Player/Inventory/Scripts/InventoryItem.gd
@@ -7,6 +7,9 @@ class_name InventoryItem
 @export var maxAmountPrStack: int
 @export var isConsumable: bool = false
 @export var isWeapon: bool = false
+@export var isHelmet = false
+@export var isBody = false
+@export var isAccessory = false
 
 func use(player: Player) -> void:
 	pass
