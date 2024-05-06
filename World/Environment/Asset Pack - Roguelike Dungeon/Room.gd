ad3b9e49b3af219c7597e275d64744cc40ae66ad
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-05 14:18:01 -1000

    Roguelike continued
    Need to align corridor with room exits

--- World/Environment/Asset Pack - Roguelike Dungeon/Room.gd
+++ World/Environment/Asset Pack - Roguelike Dungeon/Room.gd
@@ -38,8 +38,8 @@ func _open_doors() -> void:
 func _close_entrance() -> void:
 	for entry_position in entrance.get_children():
 		print(tilemap.local_to_map(entry_position.position))
-		tilemap.set_cell(0, tilemap.local_to_map(entry_position.position), 0, Vector2i(2,7))
-		tilemap.set_cell(0, tilemap.local_to_map(entry_position.position) + Vector2i.DOWN, 0, Vector2i(7,4))
+		tilemap.set_cell(1, tilemap.local_to_map(entry_position.position), 0, Vector2i(2,7))
+		tilemap.set_cell(1, tilemap.local_to_map(entry_position.position) + Vector2i.DOWN, 0, Vector2i(7,4))
 
 
 func _spawn_enemies() -> void:
