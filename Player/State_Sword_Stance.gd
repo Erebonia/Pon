8d7f4d371edfd70dc894e646142fbeb7d2e983b5
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-07 23:23:05 -1000

    Add more FX to sword stance

--- Player/State_Sword_Stance.gd
+++ Player/State_Sword_Stance.gd
@@ -34,6 +34,7 @@ func Enter():
 	swordWaveStanceSound.play()
 	await player.animationPlayer.animation_finished
 	player.animationTree.set_active(true)
+	swordStanceAuraFX.play("default")
 	
 	#Stat Bonus
 	run.MAX_SPEED = 120
