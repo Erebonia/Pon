3b434f356dc4488411b6a7e12182895116c8aefe
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-08 21:16:01 -1000

    Weather cycle prototype

--- World/world.gd
+++ World/world.gd
@@ -1,24 +1,47 @@
-extends BaseScene
+extends Node2D
+
+@onready var camera = $World_Camera
+@onready var clouds = $World_Camera/Cloud
+@onready var raylights = $World_Camera/Raylight
+@onready var leaves = $World_Camera/Leaf
+@onready var rain = $World_Camera/Rain
 
 var checkTime = DayAndNight.get_child(1)
-@onready var camera = $World_Camera
+var savedHour: int
 
 func _ready():
-	super()
-	#camera.follow_node = player
 	checkTime.connect("time_tick", Callable(self, "_on_check_time"))
 
 func _on_check_time(_day, hour, _minute):
-	#24 hour Clock
-	if (hour >= 19 and hour <= 23) or (hour >= 0 and hour < 5):
-		$Cloud.emitting = false 
-		$Raylight.emitting = false
+	#Randomize Weather
+	if savedHour < hour:
+		print("RANDOM WEATHER ACTIVE")
+		randomWeather()
+		savedHour = hour
+		
+	#Night Time
+	if (hour >= 19 and hour <= 23) or (hour >= 0 and hour < 6):
+		clouds.emitting = false 
+		raylights.emitting = false
+		leaves.emitting = false
 	else:
-		$Cloud.emitting = true 
-		$Raylight.emitting = true
+		clouds.emitting = true 
+		raylights.emitting = true
+
+func randomWeather():
+	var randomPick = randi_range(1,2)
+	print(randomPick)
+	match randomPick:
+		1:
+			leaves.emitting = true
+			rain.emitting = false
+		2:
+			leaves.emitting = false
+			rain.emitting = true
 
 func _on_inventory_gui_opened():
 	get_tree().paused = true
 
 func _on_inventory_gui_closed():
 	get_tree().paused = false
+
