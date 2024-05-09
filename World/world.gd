7dbdbd044c491825b62131ffede437990c25121e
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-08 21:41:10 -1000

    Weather adjustments / refactors

--- World/world.gd
+++ World/world.gd
@@ -7,17 +7,22 @@ extends Node2D
 @onready var rain = $World_Camera/Rain
 
 var checkTime = DayAndNight.get_child(1)
+var savedDay: int
 var savedHour: int
 
 func _ready():
 	checkTime.connect("time_tick", Callable(self, "_on_check_time"))
 
-func _on_check_time(_day, hour, _minute):
-	#Randomize Weather
-	if savedHour < hour:
-		print("RANDOM WEATHER ACTIVE")
+func _on_check_time(day, hour, _minute):
+	# Randomize Weather every 12 hours
+	if savedHour == -1 or hour == (savedHour + 12) % 24:
+		print("Hour: " + str(hour))
 		randomWeather()
 		savedHour = hour
+	
+	# Update savedDay at midnight
+	if savedDay != day:
+		savedDay = day
 		
 	#Night Time
 	if (hour >= 19 and hour <= 23) or (hour >= 0 and hour < 6):
@@ -29,15 +34,20 @@ func _on_check_time(_day, hour, _minute):
 		raylights.emitting = true
 
 func randomWeather():
-	var randomPick = randi_range(1,2)
-	print(randomPick)
+	var randomPick = randi_range(1,3)
 	match randomPick:
 		1:
+			print("Leaves Day")
 			leaves.emitting = true
 			rain.emitting = false
 		2:
+			print("Rainy Day")
 			leaves.emitting = false
 			rain.emitting = true
+		3,4:
+			print("Nothing Day")
+			leaves.emitting = false
+			rain.emitting = false
 
 func _on_inventory_gui_opened():
 	get_tree().paused = true
