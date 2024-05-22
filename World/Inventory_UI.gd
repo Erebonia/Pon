db2e047d5a2257e4787b5c6af510665ccff21b71
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-05-21 18:36:13 -1000

    Fixing editor across git

--- World/Inventory_UI.gd
+++ World/Inventory_UI.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 class_name UserInterface
 
-@onready var inventory : Inventory_gui = $InventoryGui
+@onready var inventory : Inventory_Gui = $InventoryGui
 @onready var hotbar : Hotbar = $Hotbar
 @onready var playerStatusUI : Player_Status_UI = $"Status Screen"
 
